87a16f748b46fa3ff920cd159343491e97d0ae6e
joshliddi15 <92556139+joshliddi15@users.noreply.github.com>
2023-09-28 20:41:11 -0600

    Added image to MainMenu

---
+++ MegaDesk 1.0/MainMenu.cs
@@ -0,0 +1,20 @@
+namespace MegaDesk_1._0
+{
+    public partial class MainMenu : Form
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
